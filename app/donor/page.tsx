8e71adbc800aa7d7f5660988d2c4ba4e4c2ceace
Rasel Miah <rasel6041@gmail.com>
2024-09-04 05:24:51 +0100

    testing table

---
+++ app/donor/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Table from '../search/Table'
+
+const Donor = () => {
+  return (
+    <div className='px-80'>
+        <Table />
+    </div>
+  )
+}
+
+export default Donor
