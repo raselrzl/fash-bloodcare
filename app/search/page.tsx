009e81f3b236abb840c7e852bd24e000d0b44b9b
Rasel Miah <rasel6041@gmail.com>
2024-09-04 05:58:42 +0100

    testing env

--- app/search/page.tsx
+++ app/search/page.tsx
@@ -14,9 +14,9 @@ const SearchPage: React.FC = () => {
   const fetchUsers = async () => {
     setLoading(true);
     try {
-      const response = await fetch('/api/userdata', { cache: 'no-store' });
+      const response = await fetch(`${process.env.NEXT_PUBLIC_BASE_API_URL}/api/userdata`, { cache: 'no-store' });
       console.log('BASE_API_URL in production:', process.env.NEXT_PUBLIC_BASE_API_URL);
-      console.log('BASE_API_URL in production:', process.env.BASE_API_URL);
+      console.log('BASE_API_URL:', process.env.BASE_API_URL);
       if (!response.ok) {
         throw new Error('Failed to fetch data');
       }
