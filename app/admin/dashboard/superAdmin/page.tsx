74da46aa04c2e988df432d57eb627354314a5979
Rasel Miah <rasel6041@gmail.com>
2024-09-30 21:36:20 +0100

    trying ssr

--- app/admin/dashboard/superAdmin/page.tsx
+++ app/admin/dashboard/superAdmin/page.tsx
@@ -1,11 +1,18 @@
 "use client";
-import React, { useEffect } from "react";
+import React, { useEffect, useState } from "react";
 import { useRouter } from "next/navigation";
-import AdminUsersList from "@/app/components/AdminUserList";
+import dynamic from "next/dynamic";
 import Link from "next/link";
+
+// Dynamically import the AdminUsersList component
+const AdminUsersList = dynamic(() => import("@/app/components/AdminUserList"), {
+  ssr: false, // Disables SSR for this component
+  loading: () => <p>Loading Admin Users...</p>, // Loading placeholder while component is being loaded
+});
 
 const SuperAdmin = () => {
   const router = useRouter();
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
 
   useEffect(() => {
     const isLoggedIn = localStorage.getItem("isLoggedIn");
@@ -13,10 +20,15 @@ const SuperAdmin = () => {
 
     // Check if user is not logged in or not a super admin
     if (isLoggedIn !== "true" || superAdminLoggedIn !== "true") {
-      // Redirect to another page (e.g., admin dashboard)
       router.push("/admin/dashboard");
+    } else {
+      setIsAuthenticated(true);
     }
   }, [router]);
+
+  if (!isAuthenticated) {
+    return <p>Redirecting...</p>; // You can display a loading state or message while checking authentication
+  }
 
   return (
     <div>
@@ -27,10 +39,11 @@ const SuperAdmin = () => {
           href="/admin/dashboard"
           className="bg-green-500 text-white my-10 px-6 py-2 text-lg font-bold hover:bg-green-600 transition duration-300 ease-in-out justify-center items-center"
         >
-          Click here Go to Admin Dashboard
+          Click here to go to Admin Dashboard
         </Link>
       </div>
 
+      {/* Dynamically loaded AdminUsersList component */}
       <AdminUsersList />
     </div>
   );
